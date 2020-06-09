061de5b21b3bf7095029a7ffad06ffdd471c76cb
Zachary Mayfield <zakmayfield@Zacharys-MacBook-Pro.local>
2020-06-09 11:42:01 -0400

    added post to store

--- api-trinkets/trinkets-model.js
+++ api-trinkets/trinkets-model.js
@@ -1,9 +1,15 @@
 const db = require('../data/dbConfig.js')
 
 module.exports = {
-  getTrinkets
+  getTrinkets,
+  addToStore
 }
 
 function getTrinkets() {
     return db('trinkets')
+}
+
+function addToStore(item) {
+    return db('trinkets')
+      .insert(item)
 }
